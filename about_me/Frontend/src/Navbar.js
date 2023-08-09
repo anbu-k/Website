80c87e310b917e1b99f7d47c77e651ae839c2d07
anbu-k <anbukrishnan52@gmail.com>
2023-08-08 21:39:09 -0500

    small updates

--- about_me/Frontend/src/Navbar.js
+++ about_me/Frontend/src/Navbar.js
@@ -7,6 +7,14 @@ const Navbar = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  const handleNavLinkClick = (id) => {
+    const yOffset = -70; // height of the navbar (adjust this value according to your actual navbar height)
+    const element = document.getElementById(id);
+    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+    window.scrollTo({ top: y, behavior: 'smooth' });
+  };
+
   return (
     <BootstrapNavbar
       style={{
@@ -26,13 +34,13 @@ const Navbar = () => {
       <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
       <BootstrapNavbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} smooth to="#about" style={{ color: "white" }}>
+          <Nav.Link onClick={() => handleNavLinkClick('about')} style={{ color: "white", cursor: "pointer" }}>
             About
           </Nav.Link>
-          <Nav.Link as={Link} smooth to="#education" style={{ color: "white" }}>
+          <Nav.Link onClick={() => handleNavLinkClick('education')} style={{ color: "white", cursor: "pointer" }}>
             Education
           </Nav.Link>
-          <Nav.Link as={Link} smooth to="#projects" style={{ color: "white" }}>
+          <Nav.Link onClick={() => handleNavLinkClick('projects')} style={{ color: "white", cursor: "pointer" }}>
             Projects
           </Nav.Link>
         </Nav>
