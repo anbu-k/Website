5ef7f0fd62d859254f635771d49549f8dc6fccb3
anbu-k <anbukrishnan52@gmail.com>
2023-09-13 16:25:20 -0500

    test

--- about_me/Backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.use('/images', express.static(path.join(__dirname, 'images')));
-
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000');
-});
